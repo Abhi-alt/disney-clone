0c5631054e032753871c3c05d42c9e7a31dc02b3
Abhishek Kumar <chocolate.ladu@gmail.com>
2021-11-04 17:05:49 +0530

    Movies section start

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,14 @@ import styled from 'styled-components';
 import ImgSlider  from './ImgSlider';
 import React from 'react';
 import Viewers from './Viewers';
+import Movies from './Movies';
 
 function Home(){
     return(
         <Container>
             <ImgSlider />
             <Viewers />
+            <Movies />
         </Container>
     )
 };
